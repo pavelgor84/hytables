22c5ed4ac65c9286de656e4ac6a131d1645c3630
pavelgor84 <pavelgor@gmail.com>
2023-04-25 22:44:49 +0400

    data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data/erpquantity.json'
 
 function App() {
+
+  let header = []
+  header.push(JSON.parse(data[0]))
+
+  const out = header[0].map((el) => {
+    return (
+      <th className='table'>{el}</th>
+    )
+  })
+
+  let row = []
+
+  for (let i = 2; i < 200; i++) {
+    let temp = []
+    temp.push(JSON.parse(data[i]))
+    let r = temp[0].map((el) => {
+
+      return (
+        <td>{el}</td>
+      )
+    })
+    row.push(<tr>{r}</tr>)
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <table border="1" cellspacing="0" cellpadding="0">
+
+        <thead>
+          <tr>
+            {out}
+          </tr>
+        </thead>
+
+        <tbody>
+          {row}
+        </tbody>
+
+      </table>
+
     </div>
   );
 }
